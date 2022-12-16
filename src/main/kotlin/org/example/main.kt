115298c2481f797c7032ea65b0f0ab76a349e635
YCX-Home <ycx974577817@gmail.com>
2022-12-16 20:10:05 +0800

    1

--- src/main/kotlin/org/example/main.kt
+++ src/main/kotlin/org/example/main.kt
@@ -1,24 +1,5 @@
 package org.example
 
-import org.codehaus.jackson.map.ObjectMapper
-import java.io.File
-import java.util.zip.GZIPInputStream
-import java.util.zip.GZIPOutputStream
-
 fun main() {
-    val json = "{\n" +
-            "    \"key\": \"cdntest/418.xapk\",\n" +
-            "    \"bucket\": \"res-southeast\",\n" +
-            "    \"split\": [\n" +
-            "        {\n" +
-            "            \"key\": \"cdntest/418.xapk_split/418.xapk.0\",\n" +
-            "            \"size\": 5242880\n" +
-            "        }" +
-            "    ]\n" +
-            "}"
 
-
-    val readValue = ObjectMapper().readValue(json, MergeConfig::class.java)
-
-    println(readValue)
 }
